6d90ccaec06997e739dc9a87be2b822abb309307
The-Graze <reece2004f@gmail.com>
2024-07-10 03:45:33 +0100

    ver bump

--- ProfilePictures/PluginInfo.cs
+++ ProfilePictures/PluginInfo.cs
@@ -1,12 +1,9 @@
 ﻿namespace ProfilePictures
 {
-    /// <summary>
-    /// This class is used to provide information about your mod to BepInEx.
-    /// </summary>
     internal class PluginInfo
     {
         public const string GUID = "ProfilePictrures.ByGraze";
         public const string Name = "Profile Pictures";
-        public const string Version = "2.2.0";
+        public const string Version = "2.3.0";
     }
 }
