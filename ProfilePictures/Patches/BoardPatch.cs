319fd6241cffacdf9d0a9d7b017478fa4b6dd72f
The-Graze <reece2004f@gmail.com>
2024-07-12 22:53:43 +0100

    AHh!

--- ProfilePictures/Patches/BoardPatch.cs
+++ ProfilePictures/Patches/BoardPatch.cs
@@ -1,5 +1,7 @@
 ﻿using GorillaNetworking;
 using HarmonyLib;
+using Photon.Pun;
+using System;
 using System.Collections;
 using System.ComponentModel;
 using UnityEngine;
@@ -9,71 +11,61 @@ namespace ProfilePictures.Patches
 {
     [HarmonyPatch(typeof(GorillaPlayerScoreboardLine))]
     [HarmonyPatch("UpdateLine", MethodType.Normal)]
-    public static class BoardPatch
+    public class BoardPatch
     {
-        public static Sprite PFP;
         private static bool Prefix(GorillaPlayerScoreboardLine __instance)
         {
-            try
+            if (__instance.playerVRRig.creator.CustomProperties.ContainsKey("PFP"))
             {
-                if (__instance.playerNameVisible != __instance.playerVRRig.playerNameVisible)
+                try
                 {
-                    __instance.UpdatePlayerText();
-                }
-
-                if (Time.time > __instance.initTime + __instance.emptyRigCooldown)
-                {
-                    if (__instance.playerVRRig.photonView != null)
-                    {
-                        __instance.emptyRigCount = 0;
-                    }
-                    else
-                    {
-                        __instance.emptyRigCount++;
-                        if (__instance.emptyRigCount > 30)
-                        {
-                            GorillaNot.instance.SendReport("empty rig", __instance.linePlayer.UserId, __instance.linePlayer.NickName);
-                        }
-                    }
-                }
-                if (__instance.playerVRRig.creator.CustomProperties.ContainsKey("PFP"))
-                {
-                    if (!Plugin.AllreadySaved.ContainsKey(__instance.playerVRRig.creator))
+                    if (!Plugin.SavedPlayerList.Contains(__instance.rigContainer.photonView.Owner))
                     {
                         GorillaComputer.instance.StartCoroutine(GetTexture(__instance));
                     }
-                    else
-                    {
-                        PFP = Plugin.AllreadySaved[__instance.playerVRRig.creator];
-                    }
                     __instance.playerSwatch.color = Color.white;
-                    __instance.playerSwatch.sprite = PFP;
+                    __instance.playerSwatch.sprite = Plugin.AllreadySaved[__instance.playerVRRig.creator];
                     __instance.playerSwatch.material = __instance.playerVRRig.scoreboardMaterial;
-                }
-                else
-                {
-                    Material material = ((__instance.playerVRRig.setMatIndex != 0) ? __instance.playerVRRig.materialsToChangeTo[__instance.playerVRRig.setMatIndex] : __instance.playerVRRig.scoreboardMaterial);
-                    if (__instance.playerSwatch.material != material)
-                    {
-                        __instance.playerSwatch.material = material;
-                        __instance.playerSwatch.sprite = null;
-                    }
-                    if (__instance.playerSwatch.color != __instance.playerVRRig.materialsToChangeTo[0].color)
-                    {
-                        __instance.playerSwatch.color = __instance.playerVRRig.materialsToChangeTo[0].color;
-                    }
-                }
 
-                if (__instance.myRecorder == null)
-                {
-                    __instance.myRecorder = NetworkSystem.Instance.LocalRecorder;
-                }
-
-                if (__instance.playerVRRig != null)
-                {
-                    if (__instance.playerVRRig.remoteUseReplacementVoice || __instance.playerVRRig.localUseReplacementVoice || GorillaComputer.instance.voiceChatOn == "FALSE")
+                    if (__instance.playerNameVisible != __instance.playerVRRig.playerNameVisible)
                     {
-                        if (__instance.playerVRRig.SpeakingLoudness > __instance.playerVRRig.replacementVoiceLoudnessThreshold && !__instance.rigContainer.ForceMute && !__instance.rigContainer.Muted)
+                        __instance.UpdatePlayerText();
+                    }
+
+                    if (Time.time > __instance.initTime + __instance.emptyRigCooldown)
+                    {
+                        if (__instance.playerVRRig.photonView != null)
+                        {
+                            __instance.emptyRigCount = 0;
+                        }
+                        else
+                        {
+                            __instance.emptyRigCount++;
+                            if (__instance.emptyRigCount > 30)
+                            {
+                                GorillaNot.instance.SendReport("empty rig", __instance.linePlayer.UserId, __instance.linePlayer.NickName);
+                            }
+                        }
+                    }
+                    if (__instance.myRecorder == null)
+                    {
+                        __instance.myRecorder = NetworkSystem.Instance.LocalRecorder;
+                    }
+
+                    if (__instance.playerVRRig != null)
+                    {
+                        if (__instance.playerVRRig.remoteUseReplacementVoice || __instance.playerVRRig.localUseReplacementVoice || GorillaComputer.instance.voiceChatOn == "FALSE")
+                        {
+                            if (__instance.playerVRRig.SpeakingLoudness > __instance.playerVRRig.replacementVoiceLoudnessThreshold && !__instance.rigContainer.ForceMute && !__instance.rigContainer.Muted)
+                            {
+                                __instance.speakerIcon.enabled = true;
+                            }
+                            else
+                            {
+                                __instance.speakerIcon.enabled = false;
+                            }
+                        }
+                        else if ((__instance.rigContainer.Voice != null && __instance.rigContainer.Voice.IsSpeaking) || (__instance.playerVRRig.rigSerializer.IsLocallyOwned && __instance.myRecorder != null && __instance.myRecorder.IsCurrentlyTransmitting))
                         {
                             __instance.speakerIcon.enabled = true;
                         }
@@ -82,51 +74,41 @@ namespace ProfilePictures.Patches
                             __instance.speakerIcon.enabled = false;
                         }
                     }
-                    else if ((__instance.rigContainer.Voice != null && __instance.rigContainer.Voice.IsSpeaking) || (__instance.playerVRRig.rigSerializer.IsLocallyOwned && __instance.myRecorder != null && __instance.myRecorder.IsCurrentlyTransmitting))
-                    {
-                        __instance.speakerIcon.enabled = true;
-                    }
                     else
                     {
                         __instance.speakerIcon.enabled = false;
                     }
-                }
-                else
-                {
-                    __instance.speakerIcon.enabled = false;
-                }
 
-                if (!__instance.isMuteManual)
-                {
-                    bool isPlayerAutoMuted = __instance.rigContainer.GetIsPlayerAutoMuted();
-                    if (__instance.muteButton.isAutoOn != isPlayerAutoMuted)
+                    if (!__instance.isMuteManual)
                     {
-                        __instance.muteButton.isAutoOn = isPlayerAutoMuted;
-                        __instance.muteButton.UpdateColor();
+                        bool isPlayerAutoMuted = __instance.rigContainer.GetIsPlayerAutoMuted();
+                        if (__instance.muteButton.isAutoOn != isPlayerAutoMuted)
+                        {
+                            __instance.muteButton.isAutoOn = isPlayerAutoMuted;
+                            __instance.muteButton.UpdateColor();
+                        }
                     }
                 }
-            }
-            catch
-            {
-            }
-            return false;
-        }
 
-        public static IEnumerator GetTexture(GorillaPlayerScoreboardLine GPBLine)
-        {
-            if (Plugin.AllreadySaved.ContainsKey(GPBLine.playerVRRig.creator))
-            {
-                GPBLine.playerSwatch.sprite = Plugin.AllreadySaved[GPBLine.playerVRRig.creator];
+                catch { }
+                return false;
             }
             else
             {
-                string URL = GPBLine.playerVRRig.creator.CustomProperties["PFP"].ToString();
-                UnityWebRequest www = UnityWebRequestTexture.GetTexture(URL);
-                yield return www.SendWebRequest();
-                Texture image = ((DownloadHandlerTexture)www.downloadHandler).texture;
-                PFP = Sprite.Create((Texture2D)image, new Rect(0.0f, 0.0f, image.width, image.height), new Vector2(0.5f, 0.5f), 100.0f);
-                Plugin.AllreadySaved.AddOrUpdate(GPBLine.playerVRRig.creator, PFP);
-                GPBLine.playerSwatch.sprite = PFP;
+                __instance.playerSwatch.sprite = null;
+                return true;
+            }
+        }
+        public static IEnumerator GetTexture(GorillaPlayerScoreboardLine GPBLine)
+        {
+            string URL = GPBLine.playerVRRig.creator.CustomProperties["PFP"].ToString();
+            UnityWebRequest www = UnityWebRequestTexture.GetTexture(URL);
+            yield return www.SendWebRequest();
+            Texture image = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            Sprite PFP = Sprite.Create((Texture2D)image, new Rect(0.0f, 0.0f, image.width, image.height), new Vector2(0.5f, 0.5f), 100.0f);
+            if (GPBLine.playerVRRig.creator != PhotonNetwork.LocalPlayer)
+            {
+                Plugin.AllreadySaved.Add(GPBLine.playerVRRig.creator, PFP);
             }
         }
     }
--- ProfilePictures/Plugin.cs
+++ ProfilePictures/Plugin.cs
@@ -4,9 +4,13 @@ using GorillaNetworking;
 using HarmonyLib;
 using Photon.Pun;
 using Photon.Realtime;
+using ProfilePictures.Patches;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace ProfilePictures
 {
@@ -15,24 +19,54 @@ namespace ProfilePictures
     {
         public static ConfigEntry<String> PFPLink;
         public static Dictionary<Player, Sprite> AllreadySaved = new Dictionary<Player, Sprite>();
+        public static List<Player> SavedPlayerList;
+        public static Sprite LocalSprite;
         Plugin()
         {
             Harmony harmony = Harmony.CreateAndPatchAll(typeof(Plugin).Assembly, PluginInfo.GUID);
+        }
+
+        void Update()
+        {
+            SavedPlayerList = AllreadySaved.Keys.ToList();
         }
 
         void Start()
         {
             this.AddComponent<Net>();
             PFPLink = Config.Bind("Settings", "Profile Picture URL", "https://static.wikia.nocookie.net/gorillatag/images/7/77/Gorillapin.png/revision/latest?cb=20220223225937", "Paste the Link here for people to see ur url");
+            StartCoroutine(GetTexture(PFPLink.Value));
         }
         public class Net : MonoBehaviourPunCallbacks
         {
-            public override void OnConnectedToMaster()
+            public override void OnLeftRoom()
             {
-                var table = PhotonNetwork.LocalPlayer.CustomProperties;
-                table.AddOrUpdate("PFP", PFPLink.Value);
-                PhotonNetwork.LocalPlayer.SetCustomProperties(table);
+                AllreadySaved.Clear();
+                AllreadySaved.Add(PhotonNetwork.LocalPlayer, LocalSprite);
             }
+            public override void OnPreLeavingRoom()
+            {
+                AllreadySaved.Clear();
+                AllreadySaved.Add(PhotonNetwork.LocalPlayer, LocalSprite);
+            }
+            public override void OnPlayerLeftRoom(Player otherPlayer)
+            {
+                AllreadySaved.Remove(otherPlayer);
+                AllreadySaved.Add(PhotonNetwork.LocalPlayer, LocalSprite);
+            }
+        }
+        public static IEnumerator GetTexture(string URL)
+        {
+            var table = PhotonNetwork.LocalPlayer.CustomProperties;
+            table.AddOrUpdate("PFP", PFPLink.Value);
+            PhotonNetwork.LocalPlayer.SetCustomProperties(table);
+            UnityWebRequest www = UnityWebRequestTexture.GetTexture(URL);
+            yield return www.SendWebRequest();
+            Texture image = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            Sprite PFP = Sprite.Create((Texture2D)image, new Rect(0.0f, 0.0f, image.width, image.height), new Vector2(0.5f, 0.5f), 100.0f);
+            PFP.name = "_Local_" + PhotonNetwork.LocalPlayer.NickName;
+            LocalSprite = PFP;
+
         }
     }
 }
